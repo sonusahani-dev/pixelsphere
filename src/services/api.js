e694ac4310f9ccbea007420dec0b01593ff0f848
sonusahani-dev <sonusahani.dev@gmail.com>
2025-07-11 10:41:46 +0530

    feat: use env-based removing fallback that prevant view

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
- // ✅ Your live JSON API
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL; // ✅ Your live JSON API
 
 export const fetchPhotographers = async () => {
   try {
